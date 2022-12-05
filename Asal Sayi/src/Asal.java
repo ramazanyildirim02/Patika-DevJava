ce56e4bf480b49ed0230ab08c2bffe235f89f9f2
ramazanyildirim02 <ramazanyildirim.585858@gmail.com>
2022-12-05 23:07:10 +0300

    asal sayı

---
+++ Asal Sayi/src/Asal.java
@@ -0,0 +1,18 @@
+public class Asal {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 100; i++) {
+            boolean isAsal = true;
+
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    isAsal = false;
+                }
+            }
+            if (isAsal) {
+                System.out.println(i);
+            }
+
+        }
+    }
+}
+
