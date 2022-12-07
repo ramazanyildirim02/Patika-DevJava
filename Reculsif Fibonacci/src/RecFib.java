5a30d9a7ed4ea443f89c0dfd9a3fcff670cadd6f
ramazanyildirim02 <ramazanyildirim.585858@gmail.com>
2022-12-07 22:31:08 +0300

    rekülsif fonksiyon

---
+++ Reculsif Fibonacci/src/RecFib.java
@@ -0,0 +1,17 @@
+public class RecFib {
+       static int fib(int n){
+        if (n == 1 || n ==2 ){
+            return 1;
+        }else
+            return fib(n - 1 ) + fib(n - 2 );
+    }
+
+    public static void main(String[] args) {
+           int deger = 10;
+        while (deger != 0){
+            System.out.print(+ fib(deger) + ",");
+            deger--;
+        }
+    }
+
+}
